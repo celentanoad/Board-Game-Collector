ee639cfb88c7b31f73138495e09759190b1ca43c
Alanna Celentano <alannacelentano@alannas-mbp.lan>
2020-03-18 11:00:32 -0500

    add create, update, delete functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Game(models.Model):
     name = models.CharField(max_length=250)
-    publisher = models.CharField(max_length=250)
-    numPlayers = models.IntegerField()
+    numPlayers = models.IntegerField('Number of Players')
     description = models.TextField()
 
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'game_id': self.id})
+
+class Store(models.Model):
+    name = models.CharField(max_length=250)
+    location = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
